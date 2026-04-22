f428855c7023163bab027c7c01ca8f532c4aaf27
Angelo Pirola <angelo9871@gmail.com>
2026-04-23 00:06:57 +0200

    Add Serilog HttpContextEnricher for user and agent info
    Introduced HttpContextEnricher to enrich Serilog log events with the current user's name and "User-Agent" header by accessing the HTTP context via IHttpContextAccessor. This enhances log detail for web requests.

---
+++ src/NETWebApiTemplate/Logging/HttpContextLogEventEnricher.cs
@@ -0,0 +1,29 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace NETWebApiTemplate.Logging;
+
+public class HttpContextEnricher(IHttpContextAccessor httpContextAccessor) : ILogEventEnricher
+{
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+        var httpContext = httpContextAccessor.HttpContext;
+
+        if (httpContext is null)
+        {
+            return;
+        }
+
+        var userName = httpContext.User?.Identity?.Name;
+
+        if (!string.IsNullOrWhiteSpace(userName))
+        {
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("UserName", userName));
+        }
+
+        if (httpContext.Request?.Headers?.TryGetValue("User-Agent", out var userAgent) == true)
+        {
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("User-Agent", userAgent.ToString()));
+        }
+    }
+}
