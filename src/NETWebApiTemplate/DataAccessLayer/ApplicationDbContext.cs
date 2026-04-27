44161dd561bdeeaf7ebd2f7f98963a66bcbd71d5
Angelo Pirola <angelo9871@gmail.com>
2026-04-28 00:07:59 +0200

    Refactor ApplicationDbContext for conciseness
    Reformatted namespace declaration and updated ApplicationDbContext to use a primary constructor with expression-bodied syntax. Removed unnecessary using directive.

--- src/NETWebApiTemplate/DataAccessLayer/ApplicationDbContext.cs
+++ src/NETWebApiTemplate/DataAccessLayer/ApplicationDbContext.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace NETWebApiTemplate.DataAccessLayer;
+﻿namespace NETWebApiTemplate.DataAccessLayer;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 { }
