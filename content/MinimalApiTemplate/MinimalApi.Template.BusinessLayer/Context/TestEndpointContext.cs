3f4c6fce476502217391646237366f8f0bc30b15
Angelo Pirola <angelo9871@gmail.com>
2026-05-07 00:10:28 +0200

    Add TestEndpointContext for endpoint DI context access
    Introduced TestEndpointContext<TEndpoints> to encapsulate logger, time zone service, and HttpContext for endpoint handlers via dependency injection. Enforces TEndpoints to implement IEndpointRouteHandlerBuilder.

---
+++ content/MinimalApiTemplate/MinimalApi.Template.BusinessLayer/Context/TestEndpointContext.cs
@@ -0,0 +1,14 @@
+﻿using AppEngine.Routing;
+using AppEngine.Tools.TimeZoneService.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace MinimalApi.Template.BusinessLayer.Context;
+
+public class TestEndpointContext<TEndpoints>(ILogger<TEndpoints> logger, ITimeZoneService timeZoneService, IHttpContextAccessor accessor)
+	where TEndpoints : IEndpointRouteHandlerBuilder
+{
+	public ILogger<TEndpoints> Logger { get; } = logger;
+	public ITimeZoneService TimeZoneService { get; } = timeZoneService;
+	public HttpContext HttpContext { get; } = accessor.HttpContext!;
+}
