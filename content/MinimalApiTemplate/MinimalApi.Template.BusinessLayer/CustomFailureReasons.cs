f8c01a1e9a939b8b61d42527a56c1cf6d493d7e7
Angelo Pirola <angelo9871@gmail.com>
2026-05-07 00:10:52 +0200

    Add CustomFailureReasons static class for custom errors
    Created CustomFailureReasons.cs in the BusinessLayer namespace.
    Defines a NotAvailable constant for custom failure handling,
    incrementing from FailureReasons.GenericError. Added necessary
    using directive for operation results.

---
+++ content/MinimalApiTemplate/MinimalApi.Template.BusinessLayer/CustomFailureReasons.cs
@@ -0,0 +1,8 @@
+﻿using AppEngine.Tools.OperationResults;
+
+namespace MinimalApi.Template.BusinessLayer;
+
+public static class CustomFailureReasons
+{
+	public const int NotAvailable = FailureReasons.GenericError + 1; // int = 1001
+}
