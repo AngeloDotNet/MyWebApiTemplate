c7207384bb3301b7b7ec79fd388450b205c5d077
Angelo Pirola <angelo9871@gmail.com>
2026-06-06 09:18:17 +0200

    Remove SwaggerUI, update Scalar config and DB names
    Removed Swashbuckle.AspNetCore.SwaggerUI package reference. Refactored ScalarSettings initialization with comments for optional settings. Added a commented-out persistence UoW service line. Updated SQL connection strings to use MinimalApi.Template database names.

--- content/MinimalApiTemplate/MinimalApi.Template/Program.cs
+++ content/MinimalApiTemplate/MinimalApi.Template/Program.cs
@@ -27,27 +27,33 @@ public class Program
 		}
 		else if (toolDocumentation == ApiDocumentationTool.Scalar)
 		{
+			//scalarSettings = new ScalarSettings()
+			//{
+			//	Title = $"{builder.Environment.ApplicationName} API Reference",
+
+			//	// Optional, default is true
+			//	//DarkMode = true,
+
+			//	// Optional, default is true
+			//	//ShowSidebar = true,
+
+			//	// Optional, default is Never.
+			//	//ShowDeveloperToolsVisibility = DeveloperToolsVisibility.Never,
+
+			//	// Optional, default is Mars
+			//	Theme = ScalarTheme.BluePlanet,
+
+			//	// Optional, default is CSharp
+			//	//Target = ScalarTarget.CSharp,
+
+			//	// Optional, default is HttpClient
+			//	//Client = ScalarClient.HttpClient
+			//};
+
 			scalarSettings = new ScalarSettings()
 			{
 				Title = $"{builder.Environment.ApplicationName} API Reference",
-
-				// Optional, default is true
-				//DarkMode = true,
-
-				// Optional, default is true
-				//ShowSidebar = true,
-
-				// Optional, default is Never.
-				//ShowDeveloperToolsVisibility = DeveloperToolsVisibility.Never,
-
-				// Optional, default is Mars
 				Theme = ScalarTheme.BluePlanet,
-
-				// Optional, default is CSharp
-				//Target = ScalarTarget.CSharp,
-
-				// Optional, default is HttpClient
-				//Client = ScalarClient.HttpClient
 			};
 		}
 
@@ -100,6 +106,7 @@ public class Program
 			options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
 		});
 
+		//builder.Services.AddPersistenceUoW<ENTITY, int, ApplicationDbContext>();
 		builder.Services.AddOperationResult(options =>
 		{
 			options.ErrorResponseFormat = ErrorResposeFormatOR.List;
